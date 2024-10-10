9a7a46991ee624a5ddf0252a661f1304076b4d01
Mochammad Rizky Juliansyah <suryasadewa2904@gmail.com>
2024-10-10 21:31:53 +0700

    Add files via upload

--- pertemuan2/main.cpp
+++ pertemuan2/main.cpp
@@ -2,35 +2,64 @@
 
 using namespace std;
 
-int main()
-{
-    string nama;
-    int usia;
-
-    cout << "masukan nama : ";
-    cin >> nama;
-    cout << "masukan usia : ";
-    cin >> usia;
-
-    // pake lopping for , me looping nama
-    for (int j = 0; j < usia; j++){
-        cout << nama << endl;
+// Fungsi ganjil apa genap
+string ganjil_genap(int angka) {
+    if (angka % 2 == 0) {
+    return "genap";
     }
-
-    // pake lopping while , me looping nama
-    int k = 0;
-    while (k < usia) {
-    cout << nama << endl;
-    k++;
-  }
-
-  // pake lopping do while , me looping nama
-  int l = 0;
-    do {
-    cout << nama << endl;
-    l++;
+    return "ganjil";
+}
+// Fungsi kelipatan 3
+string kelipatan_tiga(int angka) {
+    if (angka % 3 == 0) {
+    return "kelipatan 3";
     }
-    while (l < usia);
-
+    return "bukan kelipatan 3";
+}
+int main() {
+    int angka;
+    cout << "masukkan bilangannya: ";
+    cin >> angka;
+    // TAMPILAN GANJIL/GENAP
+    cout << angka << " adalah bilangan " << ganjil_genap(angka) << endl;
+    // TAMPILAN KELIPATAN 3
+    cout << angka << " itu " << kelipatan_tiga(angka) << endl;
     return 0;
 }
+
+
+
+/* CONTOH KALO FUNGSI MAX SAMA GANJIL GENAP DI PADU */
+
+
+/*#include <iostream>
+using namespace std;
+// ini maximum
+int max(int a, int b){
+    if (a > b){
+    return a;
+    }
+    return b;
+}
+// ini tentukan ganjil genap
+string ganjil_genap(int hasil) {
+    if (hasil % 2 == 0) {
+    return "genap";
+    }
+    return "ganjil";
+}
+int main()
+{
+    int x;
+    int y;
+    cout << "masukan angka pertama : ";
+    cin >> x;
+    cout << "masukan angka kedua : ";
+    cin >> y ;
+    // tentukan bilangan terbesar nya
+    int terbesar = max(x, y);
+    cout << "Bilangan terbesar: " << terbesar << endl;
+    // Menampilkan apakah bilangan terbesar ganjil atau genap
+    cout << terbesar << " adalah bilangan " << ganjil_genap(terbesar) << endl;
+    return 0;
+}*/
